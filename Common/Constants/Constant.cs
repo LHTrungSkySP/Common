c811da458bf6954df4f981c27c6fc35be1297907
LHTrungSkySP <lehoangtrung038582@gmail.com>
2024-05-16 02:14:04 +0700

    new AppException

---
+++ Common/Constants/Constant.cs
@@ -0,0 +1,29 @@
+﻿namespace Common.Constants;
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
+public static class GenderExtension
+{
+    public static List<string> TextValues = new List<string>()
+    {
+        "Nam",
+        "Nữ",
+        "Khác"
+    };
+    public static string GetText(this Gender gender)
+    {
+        try
+        {
+            return TextValues[(int)gender];
+        }
+        catch
+        {
+            return "n/a";
+        }
+    }
+}
+
--- Common/Exceptions/AppException.cs
+++ Common/Exceptions/AppException.cs
@@ -9,10 +9,42 @@ namespace Common.Exceptions
 {
     public class AppException: Exception
     {
+        public ExceptionCode Code { get; }
+
+        public IEnumerable<string> Errors { get; }
+        public IEnumerable<ErrorDetail> ErrorDetails { get; }
+
         public AppException() { }
         public AppException(string message) : base(message) { }
         public AppException(string message, params object[] args) : base(String.Format(CultureInfo.CurrentCulture, message, args))
         {
+
+        }
+        public AppException(ExceptionCode code, string message, IEnumerable<ErrorDetail>? errorDetails = null) : base("")
+        {
+            Code = code;
+            Errors = new List<string>() { message };
+            ErrorDetails = errorDetails ?? new List<ErrorDetail>();
+        }
+
+
+    }
+    public enum ExceptionCode
+    {
+        Invalidate = 1,
+        Notfound = 2,
+        Duplicate = 3,
+    }
+    public class ErrorDetail
+    {
+        public string Key { get; }
+        public dynamic Value { get; }
+
+        public ErrorDetail(string key, dynamic value)
+        {
+            Key = key;
+            Value = value;
         }
     }
+
 }
